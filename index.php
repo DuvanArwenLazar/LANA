82476db36ccfe509961166ec02dcd791a9e1df5b
DuvanArwenLazar <3dtuningduvan@gmail.com>
2022-10-14 10:17:41 -0500

    Lana

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Lana </title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="./Public/Css/styles.css">
+    <!-- <link rel="stylesheet" href="./Public/Css/bootstrap.min.css"> -->
+</head>
+<body>
+    <div class="web">
+        <header class="header-g">
+            <nav class="btn-g">
+                <ul class="content-l">
+                    <li class="l-element logo"><h2> Lana </h2></li>
+                    <li class="l-element link link-login"><a href="#" class="link link-login"> Login </a>or</li>
+                    <li class="l-element btn btn-register"><button class="btn btn-register"> Register </button></li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</body>
+</html>
